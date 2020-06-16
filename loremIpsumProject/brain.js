bb0646be1b03bc2e0ba7fac4078e1e7cc6266a4f
Priyanshu sharma <priyanshushrama709@gmail.com>
2020-06-16 15:58:41 +0530

    Add files via upload

---
+++ loremIpsumProject/brain.js
@@ -0,0 +1,71 @@
+let LoremPara = [
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Non repellendus praesentium voluptas temporibus in itaque consequuntur quos eos. Quam est mollitia nemo quis ipsum quibusdam repellendus. Laboriosam adipisci excepturi asperiores blanditiis deserunt quo ratione repellat reprehenderit voluptatum non numquam harum exercitationem dolorem nam labore aliquam, commodi dolor architecto facilis reiciendis! Nulla explicabo in, doloribus incidunt ab nostrum tenetur ducimus voluptas. Consectetur necessitatibus impedit qui mollitia maiores consequuntur provident nobis officia?Lorem ipsum dolor sit amet consectetur adipisicing elit. Id voluptatibus neque placeat perspiciatis ullam eligendi animi enim commodi laborum dolores ut, rem impedit dicta in officia facilis illum corporis mollitia iste. Vel assumenda adipisci repellendus necessitatibus animi quas repudiandae perferendis quia dolore eos non error aspernatur, exercitationem delectus. Consequuntur, consectetur!`,
+
+    `Scratch my tummy actually i hate you now fight me rub face on owner meowwww. Fight an alligator and win. Check cat door for ambush 10 times before coming in i like cats because they are fat and fluffy good now the other hand, too and man running from cops stops to pet cats, goes to jail being gorgeous with belly side up for groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked. Peer out window, chatter at birds, lure them to mouth stretch meow meow, i tell my human but love me!, i can haz meow meow, i tell my human cat sit like bread. Funny little cat chirrup noise shaking upright tail when standing next to you pee in the shoe skid on floor, crash into wall side-eyes your "jerk" other hand while being petted . Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life kitty poochy for give attitude, and find box a little too small and curl up with fur hanging out so caticus cuteicus eat fish on floor i love cuddles.`,
+
+
+    `Ignore the squirrels, you'll never catch them anyway scratch at door to be let outside, get let out then scratch at door immmediately after to be let back in. Who's the baby stinky cat or swat turds around the house when owners are asleep, cry for no apparent reason. Enslave the hooman. Chase dog then run away sleep all day whilst slave is at work, play all night whilst slave is sleeping shove bum in owner's face like camera lens but eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap. Leave hair on owner's clothes licks paws for purrr purr littel cat, little cat purr purr have a lot of grump in yourself because you can't forget to be grumpy and not be like king grumpy cat hopped up on catnip. Sleep all day whilst slave is at work, play all night whilst slave is sleeping. Jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed. Run off table persian cat jump eat fish poop on couch and fight an alligator and win yet show belly for poop on couch or attack like a vicious monster or do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life.`,
+
+
+    `Ask to go outside and ask to come inside and ask to go outside and ask to come inside i rule on my back you rub my tummy i bite you hard lick master's hand at first then bite because im moody or i like fish licks paws. Somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock enslave the hooman and slap the dog because cats rule love to play with owner's hair tie so the fat cat sat on the mat bat away with paws. Meow meow, i tell my human kitty scratches couch bad kitty and stinky cat massacre a bird in the living room and then look like the cutest and most innocent animal on the planet but attack feet. Cat slap dog in face lick yarn hanging out of own butt my water bowl is clean and freshly replenished, so i'll drink from the toilet. If it fits i sits scratch at door to be let outside, get let out then scratch at door immmediately after to be let back in yet gnaw the corn cob. Curl into a furry donut howl uncontrollably for no reason but groom forever, stretch tongue and leave it slightly out, blep. Carefully drink from water glass and then spill it everywhere and proceed to lick the puddle scratch at the door then walk away. Swat at dog purr or push your water glass on the floor. Kitty run to human with blood on mouth from frenzied attack on poor innocent mouse, don't i look cute? chase the pig around the house or floof tum, tickle bum, jellybean footies curly toes cat is love, cat is life, loves cheeseburgers for trip on catnip.`,
+
+
+    `Cat ipsum dolor sit amet, i want to go outside let me go outside nevermind inside is better cat not kitten around . Purrrrrr drink water out of the faucet yet oooo! dangly balls! jump swat swing flies so sweetly to the floor crash move on wash belly nap. Sleep all day whilst slave is at work, play all night whilst slave is sleeping is good you understand your place in my world nap all day, and stare at owner accusingly then wink for touch my tail, i shred your hand purrrr and prance along on top of the garden fence, annoy the neighbor's dog and make it bark, but cat slap dog in face. Cat sit like bread attack the dog then pretend like nothing happened sleep on dog bed, force dog to sleep on floor all of a sudden cat goes crazy there's a forty year old lady there let us feast but chew the plant. Groom forever, stretch tongue and leave it slightly out, blep purr as loud as possible, be the most annoying cat that you can, and, knock everything off the table check cat door for ambush 10 times before coming in i heard this rumor where the humans are our owners, pfft, what do they know?! or lick yarn hanging out of own butt. Miaow then turn around and show you my bum. Claws in your leg dead stare with ears cocked.`,
+
+
+    `Walk on keyboard spend six hours per day washing, but still have a crusty butthole meowzer and walk on car leaving trail of paw prints on hood and windshield and tweeting a baseball cat sit like bread. Have my breakfast spaghetti yarn be superior yet eat half my food and ask for more for take a big fluffing crap 💩 so plays league of legends paw your face to wake you up in the morning leave buried treasure in the sandbox for the toddlers. Sleep everywhere, but not in my bed while happily ignoring when being called but the door is opening! how exciting oh, it's you, meh hide head under blanket so no one can see. Meow for food, then when human fills food dish, take a few bites of food and continue meowing. Love and coo around boyfriend who purrs and makes the perfect moonlight eyes so i can purr and swat the glittery gleaming yarn to him (the yarn is from a $125 sweater) mark territory, yet pushes butt to face. Destroy couch intently stare at the same spot, yet snuggles up to shoulders or knees and purrs you to sleep. Eat all the power cords sleep. Claws in the eye of the beholder tickle my belly at your own peril i will pester for food when you're in the kitchen even if it's salad so kitty loves pigs, or stare at the wall, play with food and get confused by dust mice or meowzer. If it smells like fish eat as much as you wish meow and walk away or love you, then bite you loves cheeseburgers, and hit you unexpectedly and purr pelt around the house and up and down stairs chasing phantoms.`,
+
+
+    `Consequat ratione and explicabo yet consequuntur officia, vel, nisi. Dolore voluptatem odit so velitesse cillum irure or nostrum. Ea ipsa ab inventore and anim tempor but amet. Totam rem id. Molestiae adipisci error or magnam. Perspiciatis perspiciatis tempor sed yet laboriosam, yet inventore. Aliqua nulla or sunt or suscipit. Suscipit. Qui totam so tempor, for inventore but adipisicing but voluptas, so reprehenderit. Aute quaerat laboris eos, yet corporis. Excepteur tempora iste commodo, so autem qui and totam. Aliqua inventore ipsum, ipsum. Nulla. Aut irure so error iste, or velitesse but consequuntur. Corporis voluptatem autem eaque. Aperiam aliqua for quia, voluptas dolor yet cupidatat quaerat. Rem laboriosam nesciunt and illum yet quae so veritatis or illum. Quo si and nostrum. Rem consequat so consequuntur so proident incididunt exercitation accusantium. Ea quia but ipsam eiusmod and aperiam or accusantium quaerat. Aut fugit, yet incidunt yet laboriosam and proident adipisci. Adipisci. Architecto. Deserunt aliquid pariatur and do ipsa fugit.`,
+
+
+    `Dolor duis mollit, iste or incidunt culpa. Id eaque and natus, or incididunt. Aut illo. Ullam consectetur and consequuntur consequatur laboris, or illum, or velitesse. Omnis aliquam and labore. Eaque. Voluptatem dicta ullam tempora. Elit eiusmod eum lorem but culpa elit enim. Laboris labore. Amet. Laudantium irure consequatur, do dicta. Magna vel for fugiat so unde enim aliquam or iste. Deserunt ut and magna but omnis vel. Proident amet dolorem inventore mollit adipisicing. Vel laudantium, illo, but quis, but minim. Voluptas in or adipisci.`,
+
+
+
+    `Munchkin. American shorthair balinese kitten so cornish rex and sphynx for himalayan british shorthair. Grimalkin siamese. Donskoy lynx. American bobtail abyssinian but mouser british shorthair. Cornish rex maine coon. Puma himalayan for lion, tabby cougar yet ocicat. Thai turkish angora, american shorthair for sphynx and burmese and birman. Kitty himalayan so siamese. Maine coon lion yet devonshire rex. Scottish fold panther or egyptian mau. Munchkin. Scottish fold leopard panther so burmese so balinese cheetah. Grimalkin manx yet tabby or manx. Burmese havana brown.`,
+
+
+
+
+    `Siberian cheetah for cornish rex kitty lynx balinese . Bobcat cheetah but thai. Siberian persian devonshire rex but egyptian mau but kitty. Scottish fold panther yet tiger. Balinese . Ragdoll jaguar, tiger. Tiger balinese for turkish angora. Havana brown puma. Havana brown cornish rex jaguar maine coon or sphynx american bobtail. Manx lion ragdoll manx donskoy for egyptian mau. Ocicat. Panther malkin ocelot or american shorthair. Kitten scottish fold bengal but bobcat ocicat, and egyptian mau for russian blue. Cheetah donskoy for tom manx tomcat. Tabby ragdoll bobcat. Manx bengal or tomcat. Mouser tiger. Mouser. Cornish rex. Donskoy. Donskoy american bobtail for burmese abyssinian sphynx. Burmese cheetah burmese and jaguar so kitten, but bobcat. Kitty siamese.Birman american bobtail for malkin egyptian mau. Bombay lion for bombay for mouser havana brown or birman puma. Turkish angora manx malkin. Turkish angora lion so jaguar so bengal leopard and norwegian forest. Singapura. Ocicat donskoy lynx. Lynx russian blue so ragdoll singapura. Bombay american bobtail lynx ocelot for burmese lion persian. Siberian turkish angora. Tom grimalkin. Puma. Grimalkin malkin. Puma ragdoll. Cornish rex manx singapura himalayan but devonshire rex thai. Persian leopard and donskoy. Lion norwegian forest. Thai persian ragdoll grimalkin kitty and thai egyptian mau. American shorthair manx cheetah, devonshire rex sphynx tomcat. Tomcat egyptian mau.`
+
+];
+
+
+
+function generateLoremIpsum(UserInput) {
+    let tempArray = LoremPara.slice(0, UserInput);
+    // console.log(tempArray)
+    tempArray = tempArray.map(function (item) {
+        return `<p>${item}</p><br/><br/>`
+    }).join("")
+    // console.log(tempArray)
+    document.querySelector(".lorem-text").innerHTML = tempArray;
+
+}
+
+
+
+
+document.querySelector("form").addEventListener("click", (e) => {
+    e.preventDefault()
+    if (e.target.classList.contains('btn')) {
+        let InputBox = document.querySelector("input");
+        let amount = parseInt(InputBox.value);
+        if (isNaN(amount) || amount < 0 || amount > 10) {
+            if (amount === 0) {
+
+                document.querySelector(".lorem-text").innerHTML = "";
+            }
+            else {
+                let random = Math.floor(Math.random() * LoremPara.length)
+                document.querySelector(".lorem-text").innerHTML = `<p>${LoremPara[random]}</p>`
+            }
+
+        }
+        else {
+            generateLoremIpsum(amount)
+        }
+    }
+})
